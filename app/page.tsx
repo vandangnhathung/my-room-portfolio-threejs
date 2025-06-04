a19f047df285e6ec75383df7541438ff32784930
vandangnhathung <vandangnhathung@gmail.com>
2025-06-04 13:30:45 +0700

    feat: load all models and textures

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import { Suspense } from "react";
 
 export default function Home() {
   return (
-    <Canvas className="canvas h-full w-full" shadows camera={{ position: [1.5, 1, 1.4], fov: 55 }}>
+    <Canvas className="canvas h-full w-full" shadows camera={{ position: [1, 1, 1], fov: 65 }}>
       <color attach="background" args={["#ececec"]} />
       <Suspense fallback={null}>
         <Experience />
--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,16 +1,23 @@
-import { Environment, OrbitControls } from "@react-three/drei";
+import { Environment, OrbitControls, PerspectiveCamera } from "@react-three/drei";
 import { MyRoom } from "./MyRoom";
 
 export const Experience = () => {
   return (
     <>
       <OrbitControls />
+      <PerspectiveCamera 
+        makeDefault 
+        position={[5, 5, 5]} 
+        fov={45}
+        near={0.1}
+        far={1000}
+      />
       <group>
-      <Environment preset="sunset" />
-      <mesh>
-        <MyRoom />
-      </mesh>
-    </group>
+        <Environment preset="sunset" />
+        <mesh>
+          <MyRoom />
+        </mesh>
+      </group>
     </>
   );
 };

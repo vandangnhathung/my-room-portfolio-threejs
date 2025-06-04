2b7a31bfc6bac5ec93db7ba90be3b9bab18d6df3
vandangnhathung <vandangnhathung@gmail.com>
2025-06-04 16:35:56 +0700

    feat: setup camera and orbitcontrols

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,38 @@
 "use client";
 
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useFrame } from "@react-three/fiber";
 import { Experience } from "@/components/Experience";
 import { Suspense } from "react";
+import { useThree } from "@react-three/fiber";
+import * as THREE from "three";
+
+function CameraLogger() {
+  const { camera } = useThree();
+  
+  useFrame(() => {
+    const isPerspectiveCamera = camera instanceof THREE.PerspectiveCamera;
+    console.log('Camera Details:', {
+      position: {
+        x: camera.position.x.toFixed(2),
+        y: camera.position.y.toFixed(2),
+        z: camera.position.z.toFixed(2)
+      },
+      rotation: {
+        x: camera.rotation.x.toFixed(2),
+        y: camera.rotation.y.toFixed(2),
+        z: camera.rotation.z.toFixed(2)
+      },
+      ...(isPerspectiveCamera && { fov: (camera as THREE.PerspectiveCamera).fov }),
+      zoom: camera.zoom
+    });
+  });
+
+  return null;
+}
 
 export default function Home() {
   return (
-    <Canvas className="canvas h-full w-full" shadows camera={{ position: [1, 1, 1], fov: 65 }}>
+    <Canvas className="canvas h-full w-full" camera={{ position: [-2.74, 9.25, -3.10], rotation: [-2.39, -0.82, -2.54], fov: 75, near: 0.1, far: 1000, zoom: 1 }}>
       <color attach="background" args={["#ececec"]} />
       <Suspense fallback={null}>
         <Experience />
--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,23 +1,18 @@
-import { Environment, OrbitControls, PerspectiveCamera } from "@react-three/drei";
+import { Environment, OrbitControls } from "@react-three/drei";
 import { MyRoom } from "./MyRoom";
+import { Suspense } from "react";
+
 
 export const Experience = () => {
+  
   return (
     <>
-      <OrbitControls />
-      <PerspectiveCamera 
-        makeDefault 
-        position={[5, 5, 5]} 
-        fov={45}
-        near={0.1}
-        far={1000}
-      />
+    <Suspense fallback={null}>
       <group>
         <Environment preset="sunset" />
-        <mesh>
           <MyRoom />
-        </mesh>
       </group>
+    </Suspense>
     </>
   );
 };
